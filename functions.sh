f2b9ccf2ba102232a33dc773031ba0f6cc49845e
Dorin Paslaru <dpaslaru@cloudbasesolutions.com>
2014-12-12 09:10:47 +0200

    YAML parsing function
    Added function for reading yaml in bash

--- functions.sh
+++ functions.sh
@@ -251,6 +251,26 @@ function trueorfalse {
 }
 
 
+# Read a yaml file:
+# eval $(parse_yaml somefilename.yml "yaml_")
+# access yaml content:
+# echo $yaml_level1_level2
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
 # Control Functions
 # =================
 
